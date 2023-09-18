ac15e4afec4f8b7721459754a947d447ba34f11c
dineshbabuvaddineni <dineshbabuvaddineni@gmail.com>
2023-09-19 02:02:20 +0530

    first commit

---
+++ src/components/model_trainer.py
@@ -0,0 +1,40 @@
+import numpy as mp
+import pandas as pd
+from sklearn.linear_model import LinearRegression,Ridge,Lasso,ElasticNet
+from src.exception import CustomException
+from src.logger import logging
+
+from src.utils import save_object
+from src.utils import evaluate_model
+
+from dataclasses import dataclass
+import sys
+import os
+
+@dataclass
+class ModelTrainerConfig:
+    trained_model_file_path= os.path.join('artifacts','model.pkl')
+
+class ModelTrainer:
+    def __init__(self):
+        self.model_trainer_config=ModelTrainerConfig()
+
+    def initiate_model_training(self,train_array,test_array):
+        try:
+            logging.info('Splitting Dependent and Independent variables from train and test data')
+            xtrain, ytrain, xtest,ytest=(
+                train_array[:,:-1],
+                train_array[:,-1],
+                test_array[:,:-1],
+                test_array[:,-1]
+            )
+
+            models={
+            'LinearRegression':LinearRegression(),
+            'Lasso':Lasso(),
+            'Ridge':Ridge(),
+            'Elasticnet':ElasticNet()
+        }
+        except Exception as e:
+            logging.ino('Exception occured at Model Training')
+            raise CustomException(e,sys)
